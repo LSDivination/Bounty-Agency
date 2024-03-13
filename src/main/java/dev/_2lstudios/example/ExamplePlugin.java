7f7b80ac7917ac96ce5177afd9d90ca8be4f3c13
linsaftw <linsaftw@users.noreply.github.com>
2024-03-13 15:01:47 -0300

    done with the example test command, fully working

--- src/main/java/dev/_2lstudios/example/ExamplePlugin.java
+++ src/main/java/dev/_2lstudios/example/ExamplePlugin.java
@@ -1,5 +1,11 @@
 package dev._2lstudios.example;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dev._2lstudios.example.commands.ExampleCommand;
@@ -15,9 +21,6 @@ public class ExamplePlugin extends JavaPlugin {
 
         // Set static instance
         ExamplePlugin.instance = this;
-
-        // Register the example command
-        this.getCommand("example").setExecutor(new ExampleCommand());
         
         // Register the example listener
         this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
@@ -25,6 +28,31 @@ public class ExamplePlugin extends JavaPlugin {
         // Register the example task
         final long taskRepeatEvery = this.getConfig().getInt("task-repeat-every") * 20L;
         this.getServer().getScheduler().runTaskTimer(this, new ExampleTask(), taskRepeatEvery, taskRepeatEvery);
+    
+        // Command register example
+        Field bukkitCommandMap;
+        try {
+            bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+
+            bukkitCommandMap.setAccessible(true);
+            Object commandMap = (Object) bukkitCommandMap.get(Bukkit.getServer());
+
+            commandMap.getClass().getMethod("register", String.class, Command.class).invoke(commandMap, "seen", new CommandSeen());
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     private static ExamplePlugin instance;
