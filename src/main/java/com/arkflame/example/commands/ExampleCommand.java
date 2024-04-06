6eac2412db9195665f404872420221f50e89b6a6
linsaftw <linsaftw@users.noreply.github.com>
2024-04-06 10:34:42 -0300

    send config message on command

--- src/main/java/com/arkflame/example/commands/ExampleCommand.java
+++ src/main/java/com/arkflame/example/commands/ExampleCommand.java
@@ -2,6 +2,7 @@ package com.arkflame.example.commands;
 
 import org.bukkit.command.CommandSender;
 
+import com.arkflame.example.ExamplePlugin;
 import com.arkflame.modernlib.commands.ModernArguments;
 import com.arkflame.modernlib.commands.ModernCommand;
 
@@ -12,6 +13,7 @@ public class ExampleCommand extends ModernCommand {
 
     @Override
     public void onCommand(CommandSender sender, ModernArguments args) {
+        String label = args.getLabel();
         String arg1 = args.getText(0);
         int arg2 = args.getNumber(1);
 
@@ -25,7 +27,9 @@ public class ExampleCommand extends ModernCommand {
         
         if (!args.hasArg()) {
             sender.sendMessage("You wrote no arguments");
-            sender.sendMessage("Usage: /example [text] [number]");
+            sender.sendMessage("Usage: /" + label + " [text] [number]");
         }
+
+        sender.sendMessage(ExamplePlugin.getInstance().getMsg().getText("messages.from-command"));
     }
 }
