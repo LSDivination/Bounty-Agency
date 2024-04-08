1df7bd835ba71b5a0246f2a6e260585aa99da36b
linsaftw <linsaftw@users.noreply.github.com>
2024-04-08 19:31:51 -0300

    new libs

--- src/main/java/com/arkflame/modernlib/utils/Materials.java
+++ src/main/java/com/arkflame/modernlib/utils/Materials.java
@@ -3,6 +3,7 @@ package com.arkflame.modernlib.utils;
 import org.bukkit.Material;
 
 public class Materials {
+    private Materials() {}
 
     /**
      * Attempts to get the first valid Material from a list of names.
